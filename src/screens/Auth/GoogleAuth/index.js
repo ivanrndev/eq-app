4d48b9094d80c0bc30a98f18167d55d25d78e65f
digisdev <v.levchuk@digiscorp.com>
2021-04-02 13:27:49 +0300

    splash screen

--- src/screens/Auth/GoogleAuth/index.js
+++ src/screens/Auth/GoogleAuth/index.js
@@ -1,8 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {
   GoogleSignin,
   GoogleSigninButton,
-  statusCodes,
 } from '@react-native-google-signin/google-signin';
 import {useDispatch} from 'react-redux';
 import {authWithGoogleAccount} from '../../../actions/actions';
@@ -10,8 +9,6 @@ import {Dimensions, StyleSheet} from 'react-native';
 
 const GoogleAuth = () => {
   const dispatch = useDispatch();
-  const [userInfo, setUserInfo] = useState(null);
-  const [gettingLoginStatus, setGettingLoginStatus] = useState(true);
 
   useEffect(() => {
     GoogleSignin.configure({
@@ -27,43 +24,25 @@ const GoogleAuth = () => {
         '1064200412468-ojo80v2sod3kve8r6bul6gvlqss2p9ca.apps.googleusercontent.com',
       googleServicePlistPath: '',
     });
-    // Check if user is already signed in
-    isSignedIn();
   }, []);
 
-  const isSignedIn = async () => {
-    const isSignedIn1 = await GoogleSignin.isSignedIn();
-    setGettingLoginStatus(isSignedIn1);
-  };
   const signIn = async () => {
     try {
       await GoogleSignin.hasPlayServices();
       const userInfo = await GoogleSignin.signIn();
-
-      await dispatch(authWithGoogleAccount(userInfo.idToken));
-
-      setUserInfo({userInfo});
+      dispatch(authWithGoogleAccount(userInfo.idToken));
     } catch (error) {
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+      if (error) {
         console.log('ERR1', error);
-        // user cancelled the login flow
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        console.log('ERR2', error);
-        // operation (e.g. sign in) is in progress already
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        console.log('ERR3', error);
-        // play services not available or outdated
-      } else {
-        // some other error happened
       }
     }
   };
-  console.log('LLL', gettingLoginStatus, userInfo);
+
   return (
     <GoogleSigninButton
       style={styles.btn}
       size={GoogleSigninButton.Size.Wide}
-      color={GoogleSigninButton.Color.Dark}
+      color={GoogleSigninButton.Color.Light}
       onPress={signIn}
     />
   );
@@ -71,7 +50,8 @@ const GoogleAuth = () => {
 const styles = StyleSheet.create({
   btn: {
     height: 50,
-    width: Dimensions.get('window').width / 1.7,
+    width: Dimensions.get('window').width / 1.3,
+    maxWidth: 230,
     alignSelf: 'center',
     borderRadius: 10,
   },
