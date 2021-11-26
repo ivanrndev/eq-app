c08f82fb1a0ba587dd36b924f1cc383833d1331d
IvanHassay <ivanrndev@gmail.com>
2021-11-26 11:11:23 +0200

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  ignorePatterns: ['dist/**', '__tests__/**'],
+  rules: {
+    'no-unused-expressions': 'warn',
+    'max-len': ['error', {code: 100}],
+    'no-shadow': 'off',
+  },
 };
